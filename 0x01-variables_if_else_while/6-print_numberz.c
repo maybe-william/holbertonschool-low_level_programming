bb309165218b1bde134e506e7550c8365ebdfded
818 <818@holbertonschool.com>
2019-06-20 17:21:33 +0000

    Try file 6 first attempt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This function runs the program
+ *
+ * Return: The program exit status
+ */
+int main(void)
+{
+	int s[10];
+	int i;
+	int temp;
+
+	temp = 48;
+	for (i = 1; i <= 10; i++)
+	{
+		s[i - 1] = temp;
+		temp++;
+	}
+
+	for (i = 1; i <= 10; i++)
+	{
+		putchar(s[i - 1]);
+	}
+	putchar('\n');
+	return (0);
+}
