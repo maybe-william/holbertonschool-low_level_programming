ea7fa3a206d8f638e3c367a30482efa72108443d
818 <818@holbertonschool.com>
2019-06-20 01:05:44 +0000

    Add char pointer to temporarily store printable string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char *dora;
+
+	dora = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, dora, 59);
 	fsync(STDERR_FILENO);
 	return (1);
 }
