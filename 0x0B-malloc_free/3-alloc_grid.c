fa8cdb360bd9b6940c2143e4df1408690a1cbb57
818 <818@holbertonschool.com>
2019-07-16 16:54:17 +0000

    [LOCAL][INIT] Do first try

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a malloc'd 2d array
+ * @width: the size of array
+ * @height: the number of arrays
+ *
+ *
+ *
+ *
+ *
+ * Return: a pointer to the 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ret;
+	int *currentPointer;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	ret = (int **)malloc(height * sizeof(currentPointer));
+	if (ret == 0)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		currentPointer = (int *)malloc(width * sizeof(*currentPointer));
+		if (currentPointer == 0)
+			return (0);
+		ret[i] = currentPointer;
+		for (j = 0; j < width; j++)
+		{
+			currentPointer[j] = 0;
+		}
+	}
+	return (ret);
+}
