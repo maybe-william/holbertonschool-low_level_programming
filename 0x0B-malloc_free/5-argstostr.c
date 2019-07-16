9af829398b395af9878fa94970dc0543291a380f
818 <818@holbertonschool.com>
2019-07-16 18:22:09 +0000

    [LOCAL][INIT] Do first try

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,63 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * totallength - gets the total length including newlines
+ * @ac: the argcount
+ * @av: the args
+ *
+ * Return: the total length
+ */
+int totallength(int ac, char **av)
+{
+	int size, i, j;
+
+	size = 0;
+	for (i = 0; i < (ac - 1); i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size = size + 1;
+		}
+		size = size + 1;
+	}
+	return (size);
+}
+
+
+/**
+ * argstostr - concat all program args
+ * @ac: the number of arguments
+ * @av: the arguments
+ *
+ *
+ *
+ *
+ *
+ * Return: a pointer to the concat args
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, size, extra;
+	char *ret;
+
+	if (ac == 0 || av == 0)
+		return (0);
+
+	size = totallength(ac, av);
+	ret = (char *)malloc(size * sizeof(char));
+	if (ret == 0)
+		return (0);
+
+	extra = 0;
+	for (i = 0; i < (ac - 1); i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ret[i + j + extra] = av[i][j];
+		}
+		ret[i + j + extra] = '\n';
+		extra = extra + 1;
+	}
+	return (ret);
+}
