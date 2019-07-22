21e2bc735e530e101314785dd1e31b1bdaa4e4ff
william <191marquis@gmail.com>
2019-07-22 20:27:58 +0000

    [LOCAL][INIT] Do first try

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,13 @@
-#include "holberton.h"
+#include "dog.h"
+#include <stdlib.h>
 
 /**
- * free_dog - function description
- * @d: parameter description
+ * free_dog - set the dog free
+ * @d: the dog
  */
 void free_dog(dog_t *d)
 {
-
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
