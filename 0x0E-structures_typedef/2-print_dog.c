fa97d810961edff3c8f66c6a545de5a83a5a1c04
william <191marquis@gmail.com>
2019-07-22 19:35:13 +0000

    [LOCAL][INIT] Do first try

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,25 @@
-#include "holberton.h"
+#include "dog.h"
+#include <stdio.h>
 
 /**
- * print_dog - function description
- * @d: parameter description
+ * print_dog - print a dog
+ * @d: the dog
  */
 void print_dog(struct dog *d)
 {
+	char *nm;
+	char *own;
 
+	if (d)
+	{
+		nm = d->name;
+		own = d->owner;
+
+		if (!nm)
+			nm = "(nil)";
+		if (!own)
+			own = "(nil)";
+
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", nm, d->age, own);
+	}
 }
