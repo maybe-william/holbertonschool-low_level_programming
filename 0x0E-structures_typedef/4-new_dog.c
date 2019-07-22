2d39259ce1ec21ef2db52547e69285e0edcea4ce
william <191marquis@gmail.com>
2019-07-22 20:06:53 +0000

    [LOCAL][INIT] Do first try

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,52 @@
-#include "holberton.h"
+#include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - function description
- * @name: parameter description
- * @age: parameter description
- * @owner: parameter description
- * Return: return description
+ * new_dog - make a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: a new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d;
+	char *n;
+	char *o;
+	int sizen, sizeo, i, j;
 
+
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+
+	n = "";
+	o = "";
+	if (name)
+		n = name;
+	if (owner)
+		o = owner;
+	for (sizen = 0; n[sizen] != '\0'; sizen++)
+		;
+	for (sizeo = 0; o[sizeo] != '\0'; sizeo++)
+		;
+	d->name = malloc(sizen + 1);
+	if (!(d->name))
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeo + 1);
+	if (!(d->owner))
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i <= sizen; i++)
+		(d->name)[i] = n[i];
+	for (j = 0; j <= sizeo; j++)
+		(d->owner)[i] = o[i];
+	d->age = age;
+	return (d);
 }
