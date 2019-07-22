0e71fecbbd6ff6bdf4aa52d52398d9aa545c5206
william <191marquis@gmail.com>
2019-07-22 18:52:57 +0000

    [LOCAL][INIT] Do first try

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
