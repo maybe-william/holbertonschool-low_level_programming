1f143efb607752228899190e9ad9afcf803b8d31
william <191marquis@gmail.com>
2019-07-22 19:20:30 +0000

    [LOCAL][INIT] Do first try

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,18 @@
-#include "holberton.h"
+#include "dog.h"
 
 /**
- * init_dog - function description
- * @d: parameter description
- * @name: parameter description
- * @age: parameter description
- * @owner: parameter description
+ * init_dog - init a struct dog var
+ * @d: the pointer
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
