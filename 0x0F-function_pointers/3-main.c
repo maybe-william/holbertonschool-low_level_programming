540aa0f2c0d1e32582d4b32ba935b18d354ac897
william <191marquis@gmail.com>
2019-07-24 18:07:42 +0000

    [LOCAL][BUGFIX] Get rid of compiler warnings

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - calculator two nums.
@@ -12,9 +13,10 @@ int main(int argc, char *argv[])
 	int arg1, arg2;
 	int (*temp)(int, int);
 
+	arg1 = argc;
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 	temp = get_op_func((argv[2]));
 	printf("%i\n", temp(arg1, arg2));
-
+	return (0);
 }
