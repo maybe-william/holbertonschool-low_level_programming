cdebf59654853096fb3703b2f3af1f31eca9eb5e
william <191marquis@gmail.com>
2019-07-24 16:48:00 +0000

    [LOCAL][INIT] Do first try

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,33 @@
-#include "holberton.h"
+#include "function_pointers.h"
 
 /**
  * int_index - function description
- * @array: parameter description
- * @size: parameter description
- * Return: return description
+ * @array: the array
+ * @size: the number of elems
+ * @cmp: the comparison function
+ * Return: -1 or the index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int index, i;
 
+	index = -1;
+	if (size > 0)
+	{
+		if (cmp)
+		{
+			if (array)
+			{
+				for (i = 0; i < size; i++)
+				{
+					if (cmp(array[i]) != 0)
+					{
+						index = i;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return (index);
 }
