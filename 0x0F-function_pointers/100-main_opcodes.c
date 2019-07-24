3a5641e012238d352bd9cbcc4cc848c9c4ff5d0d
william <191marquis@gmail.com>
2019-07-24 18:48:41 +0000

    [LOCAL][INIT] Do first try

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void hack(int);
+
+/**
+ * main - does fun stuff
+ * @argc: the arg count
+ * @argv: the args
+ * Return: returns 0 or something
+ */
 int main(int argc, char *argv[])
 {
 	int num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 	num = atoi(argv[1]);
 
-	printf("Hello, world!");
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	hack(num);
+	return (0);
+}
 
+/**
+ * hack - hax
+ * @bytes: the number of bytes to print of main
+ */
+void hack(int bytes)
+{
+
+	unsigned char *x;
+	int i;
+
+	x = (unsigned char *)(&main);
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i > 0)
+			printf(" ");
+		printf("%02x", x[i]);
+	}
+	printf("\n");
 }
