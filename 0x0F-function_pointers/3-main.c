527dc2a731cf10cc54bfadcaff442c583dd0d051
william <191marquis@gmail.com>
2019-07-24 18:13:14 +0000

    [LOCAL][BUGFIX] Handle errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,10 +13,20 @@ int main(int argc, char *argv[])
 	int arg1, arg2;
 	int (*temp)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error");
+		exit(98);
+	}
 	arg1 = argc;
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 	temp = get_op_func((argv[2]));
+	if (!temp)
+	{
+		printf("Error");
+		exit(99);
+	}
 	printf("%i\n", temp(arg1, arg2));
 	return (0);
 }
