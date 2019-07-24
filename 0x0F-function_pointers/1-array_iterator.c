02050b50172e465ad25a2f6d208582563e5d26a5
william <191marquis@gmail.com>
2019-07-24 16:34:55 +0000

    [LOCAL][INIT] Do first try

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * array_iterator - function description
- * @array: parameter description
- * @size: parameter description
+ * array_iterator - do something to each thing in an array
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function to do
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int i;
 
+	if (action)
+	{
+		if (array)
+		{
+			for (i = 0; i < size; i++)
+				action(array[i]);
+		}
+	}
 }
