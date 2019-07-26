e9f1dd130031e10fa89f162e512fdb342d3a15f2
william <191marquis@gmail.com>
2019-07-26 14:00:13 +0000

    [LOCAL][INIT] Do first try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,35 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#define fori(start, end) for (i = (start); \
+i != (end); \
+(((start) < (end)) ? (i++) : (i--)))
 
 /**
- * print_numbers - function description
- * @separator: parameter description
- * @n: parameter description
+ * print_numbers - print all numbers with a string inbetween
+ * @separator: string
+ * @n: amount of params
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list vl;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(vl, n);
+
+	fori(0, n)
+	{
+		if (i && (separator))
+			printf("%s", separator);
+		printf("%i", va_arg(vl, int));
+	}
+
+	va_end(vl);
+	printf("\n");
 }
