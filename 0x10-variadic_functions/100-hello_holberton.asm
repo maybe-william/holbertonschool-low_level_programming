d7125a87230bf2aa0dcb371af1f39f07190a023f
william <191marquis@gmail.com>
2019-07-26 17:55:25 +0000

    [LOCAL][INIT] Do first try

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,15 @@
+	global main
+	section .text
+main:	mov 	rax, 1
+	mov	rdi, 1
+	mov	rsi, hello
+	mov	rdx, 18
+	syscall
+
+	mov	rax, 60
+	mov	rdi, 0
+	syscall
+
+
+	section .data
+hello:	db	"Hello, Holberton", 10
