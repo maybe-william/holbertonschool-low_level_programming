e6d16f66de49a64b46bc8381c7f1e0175262b394
william <191marquis@gmail.com>
2019-07-26 14:12:16 +0000

    [LOCAL][INIT] Do first try

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,40 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#define fori(start, end) for (i = (start); \
+i != (end); \
+(((start) < (end)) ? (i++) : (i--)))
 
 /**
- * print_strings - function description
- * @separator: parameter description
- * @n: parameter description
+ * print_strings - print all strings with a string inbetween
+ * @separator: string
+ * @n: amount of params
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	char *temp;
+	va_list vl;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(vl, n);
+
+	fori(0, n)
+	{
+		if (i && (separator))
+			printf("%s", separator);
+		temp = va_arg(vl, char *);
+		if (temp)
+			printf("%s", temp);
+		else
+			printf("(nil)");
+	}
+
+	va_end(vl);
+	printf("\n");
 }
