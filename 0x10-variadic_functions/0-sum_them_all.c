7dd5150bc977a86ff7efd4feec403813b536abc9
william <191marquis@gmail.com>
2019-07-26 13:39:25 +0000

    [LOCAL][INIT] Do first try

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,7 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#define fori(start, stop) for (i = (start); \
+i != stop; \
+((start < stop) ? (i++) : (i--)))
 
 /**
  * sum_them_all - function description
@@ -7,5 +10,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list vl;
+	unsigned int i, sum;
 
+
+	if (!n)
+		return (n);
+	sum = 0;
+	va_start(vl, n);
+	fori(0, n)
+	{
+		sum = sum + va_arg(vl, int);
+	}
+	va_end(vl);
+	return (sum);
 }
