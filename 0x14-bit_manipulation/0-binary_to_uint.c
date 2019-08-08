b22980e008f8c8ef5fe1f8d087cade5faf984fa4
william <191marquis@gmail.com>
2019-08-08 16:20:44 +0000

    [LOCAL][BUGFIX] Make betty happy

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_rec(const char *b)
 {
 	if (b[0] == '\0')
-		return 0;
+		return (0);
 	return ((binary_rec(b + 1) * 2) + (b[0] - '0'));
 }
 
@@ -25,11 +25,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	while(b[i] != '\0')
+	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
-	return binary_rec(b);
+	return (binary_rec(b));
 }
