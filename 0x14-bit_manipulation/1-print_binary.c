81ea923859a7302bc833317d919bbc1139d98b8d
william <191marquis@gmail.com>
2019-08-08 17:59:40 +0000

    [LOCAL][BUGFIX] Reverse printing direction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,31 @@
 #include "holberton.h"
 
+
+/**
+ * print_bin_rec - print a binary number recursively
+ * @n: the number to print
+ *
+ */
+void print_bin_rec(unsigned long int n)
+{
+	if (!n)
+		return;
+
+	print_bin_rec(n >> 1);
+
+	if ((n >> 1))
+		_putchar(((n >> 1) & 1) + '0');
+
+
+}
 /**
  * print_binary - function description
  * @n: parameter description
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int remainder = n, digit = 0;
-
-	while (remainder != 0)
-	{
-		digit = (remainder >> 1) & 1;
-		remainder = remainder >> 1;
-		_putchar(digit + '0');
-	}
+	if (!n)
+		_putchar('0');
+	else
+		print_bin_rec(n);
 }
