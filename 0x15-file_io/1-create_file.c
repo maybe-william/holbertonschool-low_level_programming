47b0870a1aac82229fb004584aeea22d7faa490a
william <191marquis@gmail.com>
2019-08-13 16:59:30 +0000

    [LOCAL][BUGFIX] Fix a size of zero for write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content[0] == '\0')
 	{
 		close(desc);
-		return (1);
+		return (-1);
 	}
 
 	for (textsize = 0; text_content[textsize]; textsize++)
