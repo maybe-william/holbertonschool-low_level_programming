a9425a0a0d54906d81022b24f7e6f19d558e36f1
william <191marquis@gmail.com>
2019-08-13 17:25:57 +0000

    [LOCAL][INIT] Do first try

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,39 @@
 #include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * append_text_to_file - function description
- * @filename: parameter description
- * @text_content: parameter description
- * Return: return description
+ * append_text_to_file - append text to an already existing file
+ * @filename: the file
+ * @text_content: the text to append
+ * Return: 1 if successful, -1 else
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int desc, textsize;
+	ssize_t wstat;
 
+	if (!filename)
+		return (-1);
+
+	/*first call is to create file only if non-existent*/
+	desc = open(filename, O_WRONLY | O_APPEND);
+	if (desc < 0)
+		return (-1);
+
+	if (!text_content || text_content[0] == '\0')
+	{
+		close(desc);
+		return (1);
+	}
+
+	for (textsize = 0; text_content[textsize]; textsize++)
+		;
+	wstat = write(desc, text_content, textsize);
+
+	close(desc);
+	if (wstat != textsize)
+		return (-1);
+	return (1);
 }
