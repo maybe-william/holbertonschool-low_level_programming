7c0ed4cd6287019778c79c34457ddcb95991fea9
william <191marquis@gmail.com>
2019-09-16 20:22:11 +0000

    [BUGFIX] Add to tmp directory to not have in current dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O libmine.so https://www.github.com/maybe-william/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libmine.so?raw=true/
-export LD_PRELOAD=./libmine.so
+wget -O /tmp/libmine.so https://www.github.com/maybe-william/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libmine.so?raw=true/
+export LD_PRELOAD=/tmp/libmine.so
