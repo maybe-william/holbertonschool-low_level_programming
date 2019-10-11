49fe31b48a12f6d840eb0d001c9750572dc4a11d
william <191marquis@gmail.com>
2019-10-11 14:56:49 +0000

    [INIT] Do first try

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,35 @@
-#include "holberton.h"
+#include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * hash_table_set - function description
- * @ht: parameter description
- * @key: parameter description
- * @value: parameter description
- * Return: return description
+ * hash_table_set - add a k,v pair to a hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1 if succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int ind = 0;
+	hash_node_t *x = NULL;
+	hash_node_t *current = NULL;
 
+	if (!ht)
+		return (0);
+	if (!key)
+		return (0);
+
+	x = malloc(sizeof(hash_node_t));
+	if (!x)
+		return (0);
+
+	ind = key_index((const unsigned char *)key, ht->size);
+	current = (ht->array)[ind];
+
+	x->key = (char *)key;
+	x->value = (char *)value;
+	x->next = current;
+
+	(ht->array)[ind] = x;
+	return (1);
 }
