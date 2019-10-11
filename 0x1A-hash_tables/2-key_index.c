d27afcc175866160cd0ae225a3b7fe91d31228f3
william <191marquis@gmail.com>
2019-10-11 14:41:46 +0000

    [INIT] Do first try

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,15 @@
-#include "holberton.h"
+#include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * key_index - function description
- * @key: parameter description
- * @size: parameter description
- * Return: return description
+ * key_index - get the index to store the key at in the array
+ * @key: the key to store
+ * @size: the size of the array
+ * Return: the index in the array to store the key in
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+	if (key == NULL)
+		return (0);
+	return (hash_djb2(key) % size);
 }
