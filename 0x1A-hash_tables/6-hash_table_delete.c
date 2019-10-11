6f61357130574070835c097e9786f385da733f89
william <191marquis@gmail.com>
2019-10-11 16:24:59 +0000

    [INIT] Do first try

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_delete - function description
@@ -6,5 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *ll = NULL;
+	hash_node_t *tmp = NULL;
 
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		ll = (ht->array)[i];
+		while (ll)
+		{
+			tmp = ll;
+			ll = ll->next;
+			free(tmp->value);
+			free(tmp->key);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
