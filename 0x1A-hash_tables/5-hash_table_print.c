7133c05ca1ce89eddc88efa405c181ab87309818
william <191marquis@gmail.com>
2019-10-11 15:56:57 +0000

    [INIT] Do first try

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * hash_table_print - function description
@@ -6,5 +8,28 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	int first = 1;
+	hash_node_t *x = NULL;
 
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		x = (ht->array)[i];
+		if (x != NULL)
+		{
+			if (first != 1)
+				printf(", ");
+			first = 0;
+			while (x != NULL)
+			{
+				if (x != (ht->array)[i])
+					printf(", ");
+				printf("'%s': ", x->key);
+				printf("'%s'", x->value);
+				x = x->next;
+			}
+		}
+	}
+	printf("}\n");
 }
