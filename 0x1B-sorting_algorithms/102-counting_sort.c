91541003b135916476ca00cb08383a64446f08e9
william <191marquis@gmail.com>
2019-10-29 18:22:21 +0000

    [INIT] Do first try

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -7,5 +7,38 @@
  */
 void counting_sort(int *array, size_t size)
 {
+	int *other = NULL, max = 0, acc = 0, j = 0;
+	size_t i = 0;
 
+	if (size < 2)
+		return;
+
+	max = array[0];
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	other = malloc((max + 1) * sizeof(int));
+
+	if (!other)
+		return;
+	for (i = 0; i < size; i++)
+		other[array[i]] = other[array[i]] + 1;
+
+	acc = 0;
+	for (i = 0; i < (size_t)(max + 1); i++)
+	{
+		acc = acc + other[i];
+		other[i] = acc;
+	}
+	print_array(other, max + 1);
+
+	for (i = 0; i < (size_t)(max + 1); i++)
+	{
+		while (j < other[i])
+		{
+			array[j] = i;
+			j++;
+		}
+	}
+	free(other);
 }
