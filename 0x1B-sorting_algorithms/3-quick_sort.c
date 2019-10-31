11357f6581c1973a630656e8a929f867896a35e0
william <191marquis@gmail.com>
2019-10-31 02:58:18 +0000

    [INIT] Merge quicksort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,11 +1,75 @@
-#include "holberton.h"
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * quick_sort - function description
- * @array: parameter description
- * @size: parameter description
+ * partition - Partitions array
+ * @w: w
+ * @x: x
+ * @y: y
+ * @z: z
+ * Return: return
  */
-void quick_sort(int *array, size_t size)
-{
 
+int partition(int *w, int x, int y, size_t z)
+{
+	int i = x, j = x, q, p;
+
+	for (p = w[y]; i < y; i++)
+	{
+		if (w[i] <= p)
+		{
+			q = w[i];
+			w[i] = w[j];
+			w[j] = q;
+			if (w[i] != w[j])
+			{
+				print_array(w, z);
+			}
+			j++;
+		}
+
+	}
+	w[y] = w[j];
+	w[j] = p;
+	if (w[y] != w[j])
+		print_array(w, z);
+	return (j);
+}
+
+/**
+ * recur_sort - Recursion
+ * @w: w
+ * @x: x
+ * @y: y
+ * @z: z
+ * Return: Void
+ */
+
+void recur_sort(int *w, int x, int y, size_t z)
+{
+	int m;
+
+	if (y - x <= 0)
+		return;
+	m = partition(w, x, y, z);
+	recur_sort(w, x, m - 1, z);
+	recur_sort(w, m + 1, y, z);
+}
+
+/**
+ * quick_sort - Quick
+ * @array: array
+ * @size: size
+ */
+
+void quick_sort(int *array, size_t size)
+
+{
+	int end = (int)size;
+
+	if (!array || size <= 2)
+		return;
+	end--;
+	recur_sort(array, 0, end, size);
 }
