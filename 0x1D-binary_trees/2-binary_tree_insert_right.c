46e7ae4866daeb068c869f61454f52a8e86fd990
william <191marquis@gmail.com>
2019-12-03 22:19:11 +0000

    [INIT] Do first try

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,12 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - function description
- * @parent: parameter description
- * @value: parameter description
- * Return: return description
+ * binary_tree_insert_right - insert a node as the right node
+ * @parent: the parent node
+ * @value: the value
+ * Return: the new node or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+	binary_tree_t *pright;
 
+	if (parent == NULL)
+		return (NULL);
+
+	pright = parent->right;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->right = pright;
+	new->left = NULL;
+	new->n = value;
+
+	parent->right = new;
+	if (new->right != NULL)
+		new->right->parent = new;
+	return (new);
 }
