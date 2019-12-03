937657bd4517b12cc38f56bd9c9bb9fab371084b
william <191marquis@gmail.com>
2019-12-03 22:02:33 +0000

    [INIT] Do first try

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,12 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap - function description
- * @array: parameter description
- * @size: parameter description
- * Return: return description
+ * binary_tree_node - create a new binary tree node
+ * @parent: the parent node
+ * @value: the value
+ * Return: the new node or NULL on failure
  */
-heap_t *array_to_heap(int *array, size_t size)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
 
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	if (parent == NULL)
+	{
+		return (new);
+	}
+	if (parent->left == NULL)
+	{
+		parent->left = new;
+		return (new);
+	}
+	if (parent->right == NULL)
+	{
+		parent->right = new;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,12 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function description
- * @parent: parameter description
- * @value: parameter description
- * Return: return description
+ * binary_tree_insert_left - insert a node as the left node
+ * @parent: the parent node
+ * @value: the value
+ * Return: the new node or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+	binary_tree_t *pleft;
 
+	if (parent == NULL)
+		return (NULL);
+
+	pleft = parent->left;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->left = pleft;
+	new->right = NULL;
+	new->n = value;
+
+	parent->left = new;
+	if (new->left != NULL)
+		new->left->parent = new;
+	return (new);
 }
