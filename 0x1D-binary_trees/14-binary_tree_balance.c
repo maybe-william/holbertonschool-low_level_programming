9af9c9efa31c89598a55e92e631c4b3ca93c5b1c
william <191marquis@gmail.com>
2019-12-04 17:14:01 +0000

    [INIT] Do first try

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,12 +1,41 @@
 #include "binary_trees.h"
 
+int get_h(const binary_tree_t *tree, int acc);
 /**
- * avl_remove - function description
- * @root: parameter description
- * @value: parameter description
- * Return: return description
+ * binary_tree_balance - measure the balance for a binary tree
+ * @tree: the binary tree
+ * Return: the balance factor, 0 if NULL
  */
-avl_t *avl_remove(avl_t *root, int value)
+int binary_tree_balance(const binary_tree_t *tree)
 {
+	int lh, rh;
 
+	if (tree == NULL)
+		return (0);
+
+	lh = get_h(tree->left, 0);
+	rh = get_h(tree->right, 0);
+
+	return (lh - rh);
+}
+
+/**
+ * get_h - get the height of a tree
+ * @tree: the tree
+ * @acc: an accumulator
+ * Return: the height of a tree
+ */
+int get_h(const binary_tree_t *tree, int acc)
+{
+	int tmp, a, b;
+
+	if (tree == NULL)
+		return (acc);
+	tmp = acc + 1;
+	a = get_h(tree->left, tmp);
+	b = get_h(tree->right, tmp);
+
+	if (b > a)
+		return (b);
+	return (a);
 }
