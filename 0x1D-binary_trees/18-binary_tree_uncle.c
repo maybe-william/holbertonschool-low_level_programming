d8f95908c7bdbcc99435583dc78300cfe8a34b03
william <191marquis@gmail.com>
2019-12-04 19:23:04 +0000

    [INIT] Do first time

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - function description
- * @node: parameter description
- * Return: return description
+ * binary_tree_uncle - get an uncle
+ * @node: the node to get the uncle of
+ * Return: the uncle or NULL if not possible
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL);
 
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
