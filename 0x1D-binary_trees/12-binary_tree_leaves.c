be267f04132a57faccebf04a4dd87df3ab4dd924
william <191marquis@gmail.com>
2019-12-04 16:33:43 +0000

    [INIT] Do first try

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -1,11 +1,33 @@
 #include "binary_trees.h"
 
+size_t leaf_helper(const binary_tree_t *tree);
+
 /**
- * binary_tree_leaves - function description
- * @tree: parameter description
- * Return: return description
+ * binary_tree_leaves - find the leaves of a tree
+ * @tree: the tree
+ * Return: the leaf count
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+	return (leaf_helper(tree));
+}
 
+/**
+ * leaf_helper - recursive helper with accumulator
+ * @tree: the tree
+ * Return: the leaves of a tree
+ */
+size_t leaf_helper(const binary_tree_t *tree)
+{
+	size_t a, b;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	a = leaf_helper(tree->left);
+	b = leaf_helper(tree->right);
+	return (a + b);
 }
--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,11 +1,34 @@
 #include "binary_trees.h"
 
+size_t child_helper(const binary_tree_t *tree);
+
 /**
- * binary_tree_nodes - function description
- * @tree: parameter description
- * Return: return description
+ * binary_tree_nodes - find the nodes of a tree with at least one child
+ * @tree: the tree
+ * Return: the node count
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+	return (child_helper(tree));
+}
 
+/**
+ * child_helper - recursive helper with accumulator
+ * @tree: the tree
+ * Return: the nodes of a tree with at least one child
+ */
+size_t child_helper(const binary_tree_t *tree)
+{
+	size_t tmp, a, b;
+
+	if (tree == NULL)
+		return (0);
+	tmp = 0;
+	if (tree->left || tree->right)
+		tmp = 1;
+	a = child_helper(tree->left);
+	b = child_helper(tree->right);
+	return (a + b + tmp);
 }
