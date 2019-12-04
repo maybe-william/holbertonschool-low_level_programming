6b2eb80df5c4d6b179fc9ec4a766032af8b0792a
william <191marquis@gmail.com>
2019-12-04 19:15:58 +0000

    [INIT] Do first try

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - function description
- * @node: parameter description
- * Return: return description
+ * binary_tree_sibling - return a sibling to a node
+ * @node: the node in question
+ * Return: a sibling or NULL if not possible
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
