3e14e76d52e8b4c969c0536d807da3e25a73168a
william <191marquis@gmail.com>
2019-12-05 18:20:55 +0000

    [BUGFIX] Check intermediate values also

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+#include <limits.h>
+
+int bin_help(const binary_tree_t *tree, int max, int min);
 
 /**
  * binary_tree_is_bst - check if a tree is a bst
@@ -7,21 +10,39 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int valid;
+	return (bin_help(tree, INT_MAX, INT_MIN));
+}
+
+
+
+/**
+ * bin_help - helper function for the is_bst function
+ * @tree: the current node
+ * @max: the max bound
+ * @min: the min bound
+ * Return: 1 if bst, 0 otherwise
+ */
+int bin_help(const binary_tree_t *tree, int max, int min)
+{
+	int valid, tmp;
 
 	if (tree == NULL)
 		return (0);
 
 	valid = 1;
+	valid = valid && ((tree->n - 1) < max);
+	valid = valid && ((tree->n + 1) > min);
 	if (tree->left)
 	{
 		valid = valid && (tree->left->n < tree->n);
-		valid = valid && binary_tree_is_bst(tree->left);
+		tmp = bin_help(tree->left, tree->n - 1, min);
+		valid = valid && tmp;
 	}
 	if (tree->right)
 	{
 		valid = valid && (tree->right->n > tree->n);
-		valid = valid && binary_tree_is_bst(tree->right);
+		tmp = bin_help(tree->right, max, tree->n + 1);
+		valid = valid && tmp;
 	}
 
 	return (valid);
