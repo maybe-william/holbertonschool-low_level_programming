cb7f3c6745d2f121076f8999b7112a4d4b9de6d2
william <191marquis@gmail.com>
2020-01-15 21:15:13 +0000

    [INIT] Do first try

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_search - function description
+ * jump_search - jump search algorithm
  * @array: parameter description
  * @size: parameter description
  * @value: parameter description
@@ -9,5 +10,33 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+	unsigned int m;
+	unsigned int sect;
+	unsigned int esect;
+	unsigned int j;
 
+	if (!array)
+		return (-1);
+
+	m = (int)sqrt(size);
+	sect = 0;
+	esect = 0;
+
+	while (esect < size && array[esect] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", esect, array[esect]);
+		sect = sect + 1;
+		esect = sect * m;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", esect - m, esect);
+
+	j = esect - m;
+	while (j < size && j <= esect)
+	{
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+		j++;
+	}
+	return (-1);
 }
