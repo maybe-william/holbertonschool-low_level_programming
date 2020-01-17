6ff8e114192205e4135f6546c4c7d91339c4cb5d
william <191marquis@gmail.com>
2020-01-17 00:01:21 +0000

    [INIT] Do first try

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,4 +1,21 @@
 #include "search_algos.h"
+#include <math.h>
+
+/**
+ * get_next - get next express
+ * @ptr: the pointer
+ * Return: the next express
+ */
+skiplist_t *get_next(skiplist_t *ptr)
+{
+	skiplist_t *x = ptr;
+
+	if (ptr->express)
+		return (ptr->express);
+	while (x->next)
+		x = x->next;
+	return (x);
+}
 
 /**
  * linear_skip - function description
@@ -8,5 +25,44 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
+	unsigned int size, start, end, j;
+	skiplist_t *prev;
+	skiplist_t *nex;
 
+	if (!list)
+		return (NULL);
+	prev = NULL;
+	nex = list;
+	while (nex->express)
+		nex = nex->express;
+	while (nex->next)
+		nex = nex->next;
+	size = nex->index + 1;
+	nex = list;
+	start = 0;
+	end = size - 1;
+	while ((nex->index + 1) < size && (nex->n) < value)
+	{
+		prev = nex;
+		nex = get_next(nex);
+		j = nex->index;
+		if (nex->next)
+			printf("Value checked at index [%d] = [%d]\n", j, nex->n);
+	}
+	if (prev)
+		start = prev->index;
+	end = nex->index;
+	if (prev == nex)
+		start = nex->index;
+	printf("Value found between indexes [%d] and [%d]\n", start, end);
+	j = start;
+	while (j < size && j <= end)
+	{
+		printf("Value checked at index [%d] = [%d]\n", j, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+		j++;
+	}
+	return (NULL);
 }
